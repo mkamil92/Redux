f7ac17d3e35aaeae3a94697f53cf17338dd909a9
shaheen <mkamil.bcss19@iba-suk.edu.pk>
2022-10-26 10:34:55 +0500

    sending redux

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,6 @@
 import {connect} from 'react-redux';
-function Counter(props){
+function Counter(props)
+{
     return (<>
     <button onClick={()=> props.dispatch({type:'DECREMENT_ONE'})}>Decrement</button>
     <span>Count:{props.count}</span>
